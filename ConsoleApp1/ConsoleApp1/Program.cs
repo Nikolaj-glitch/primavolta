9587b84d088a5f83e32d6f489571faca612e702d
Nikolaj-glitch <75386723+Nikolaj-glitch@users.noreply.github.com>
2022-09-19 10:38:04 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yomamaweight = 9999999;
+            Console.WriteLine("Hello World!");
+            for (int i = 0; i < yomamaweight; i++)
+            {
+                Console.WriteLine("Get a life, " + i + "Yo mama so fat she made c# crash");
+            }
+        }
+    }
+}
