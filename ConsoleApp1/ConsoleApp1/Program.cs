eef1c1b559053e4b52c3de0c1158e6b5ba7df843
Nikolaj-glitch <75386723+Nikolaj-glitch@users.noreply.github.com>
2022-09-26 10:16:42 +0200

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,14 +6,14 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int yomamaweight = 999900;
+            int yo = 999900;
             Console.WriteLine("Hello World!");
-             for (int i = 0; i < yomamaweight; i--)
+             for (int i = 0; i < yo; i--)
             {
             
                 for (int i = 0; i < yomamaweight; i+10)
                 {
-                     Console.WriteLine("Get a life, " + i + "Yo mama so fat she made c# crash");
+                     Console.WriteLine("Get a life, " + i + "Hello there");
                 }
             }
         }
